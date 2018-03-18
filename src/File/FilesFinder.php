112536b20ebfeabf5dee916a1343b3ce66314b01
Matt Campbell <matthewcampbell508@gmail.com>
2018-03-18 16:07:52 +0000

    Add class to filter changed files by given paths
    This will be used in conjunction with a `git-review.yml` file, where
    the end user can modify this config file to provide valid paths.

---
+++ src/File/FilesFinder.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace GitReview\File;
+
+use Spatie\Regex\Regex;
+use Tightenco\Collect\Support\Collection;
+
+class FilesFinder
+{
+    private $files;
+    private $pathCriteria;
+    /** @var Collection $foundFiles */
+    private $foundFiles;
+
+    public function __construct(Collection $files, array $pathCriteria = [])
+    {
+        $this->files = $files;
+        $this->pathCriteria = $pathCriteria;
+        $this->foundFiles = new Collection([]);
+
+        $this->formatCriteria();
+
+        $this->findFilesByGivenCriteria();
+    }
+
+    public function getFoundFiles(): Collection
+    {
+        return $this->foundFiles;
+    }
+
+    public function count(): int
+    {
+        return count($this->getFoundFiles());
+    }
+
+    private function findFilesByGivenCriteria()
+    {
+        $this->files->filter(function (File $file) {
+            $found = false;
+
+            foreach ($this->pathCriteria as $criteria) {
+                if (Regex::match($criteria, $file->getRelativePath())->hasMatch()) {
+                    $found = true;
+
+                    break;
+                }
+            }
+
+            return $found;
+        })->each(function (File $file) {
+            $this->addFile($file);
+        });
+    }
+
+    private function addFile($file)
+    {
+        $this->foundFiles->push($file);
+    }
+
+    private function formatCriteria()
+    {
+        $this->pathCriteria = \array_map(function ($value) {
+            $escapeChars = str_replace("/*", "/.*", $value);
+            $escapeChars = str_replace("/", "\/", $escapeChars);
+
+            return "/$escapeChars/";
+        }, $this->pathCriteria);
+    }
+}
